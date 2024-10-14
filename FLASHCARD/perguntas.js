17a5994ff5710e411c170926818fc4d4d4773077
LucaslGuedes165 <lucas301207fonseca@gmail.com>
2024-10-14 07:33:56 -0300

    Add files via upload

---
+++ FLASHCARD/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'pessoal',
+    'Qual seu jogo favorito? ',
+    'GTA RP'
+)
+
+criaCartao(
+    'pessoal',
+    'qual seu carro favorito?',
+    'koengsegg Agera RS'
+)
+
+criaCartao(
+    'recomendação',
+    'um curso',
+    'Administração ou informática'
+)
+
+criaCartao(
+    'recomendacão',
+    'um jogo',
+    'Minecraft Serv Bed Wars'
+)
